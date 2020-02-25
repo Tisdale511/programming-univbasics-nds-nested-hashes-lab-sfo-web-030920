c45c84799a6aa9827eabb19d304f21ed4ba92b11
Tisdale Fry <tisdalefry@gmail.com>
2020-02-25 02:10:34 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,13 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  literal_arr = %w[DON_G JOELLE_VD PAT_M KATE_G BRUCE_G]
+  literal_arr = %w[
+    { name:  "Don Gately", occupation: "Live-in Staff" }
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+    { name:  "Pat Monteseian", occupation: "Staff" }
+    { name:  "Kate Gompert", occupation: "None" }
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    ]
   return literal_arr
 end
 
