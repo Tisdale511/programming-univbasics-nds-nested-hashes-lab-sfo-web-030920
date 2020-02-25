e5f80beb36bccd07e97f456b469ff67f69e02187
Tisdale Fry <tisdalefry@gmail.com>
2020-02-25 02:14:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  literal_arr = %w[[DON_G JOELLE_VD PAT_M KATE_G BRUCE_G]]
+  literal_arr = %w[DON_G JOELLE_VD PAT_M KATE_G BRUCE_G]
   return literal_arr
 end
 
